f8b8565c24c623f3073c6ce62d9a334aa31f1814
maximakhmin <max-akhmin@yandex.ru>
2022-04-23 21:47:54 +0300

    tara

--- triplechess/main/logic/board.py
+++ triplechess/main/logic/board.py
@@ -3,6 +3,11 @@ import tara
 class Board:
     def __init__(self):
         self.white = []
-        tara1 = tara("A", 1)
-        tara2 = tara("H", 1)
-        self.white += tara1 + tara2
+        self.tara1 = tara.Tara("A", "1", "white")
+
+
+
+
+
+
+
--- triplechess/main/logic/tara.py
+++ triplechess/main/logic/tara.py
@@ -4,7 +4,13 @@ class Tara:
     def __init__(self, letter, number, color):
         self.letter = letter
         self.number = number
-        self.color = "red"
+        self.color = color
+        self.is_walked = False
+
+    # метод, который возвращает два массива:
+    # массив1 - массив точек, на которые можно ходить
+    # массив2 - массив точек, которые эта фигура может съесть
+    # входные данные - массивы точек вида [["A", "1"], ["E", "2"],...]
 
     def __dots__(self, white, black, red):
         numbers = []
@@ -22,8 +28,42 @@ class Tara:
             letters += consts.LETTERS_2 + consts.LETTERS_3
         if self.number in consts.NUMBERS_3:
             letters += consts.LETTERS_1 + consts.LETTERS_3
-
         dots = []
-        for i in range(letter, )
+        dots_eat = []
+        index_letter = letters.index(self.letter)
+        index_number = numbers.index(self.number)
+        for step in range(-1, 2, 2):
+            for i in range(index_letter+step, len(letters), step):
+                if [letters[i], self.number] in white:
+                    if self.color != "white":
+                        dots_eat.append([letters[i], self.number])
+                        break
+                elif [letters[i], self.number] in black:
+                    if self.color != "black":
+                        dots_eat.append([letters[i], self.number])
+                        break
+                elif [letters[i], self.number] in red:
+                    if self.color != "red":
+                        dots_eat.append([letters[i], self.number])
+                        break
+                else:
+                    dots.append([letters[i], self.number])
 
+        for step in range(-1, 2, 2):
+            for i in range(index_number+step, len(numbers), step):
+                if [self.letter, numbers[i]] in white:
+                    if self.color != "white":
+                        dots_eat.append([self.letter, numbers[i]])
+                        break
+                elif [self.letter, numbers[i]] in black:
+                    if self.color != "black":
+                        dots_eat.append([self.letter, numbers[i]])
+                        break
+                elif [self.letter, numbers[i]] in red:
+                    if self.color != "red":
+                        dots_eat.append([self.letter, numbers[i]])
+                        break
+                else:
+                    dots.append([self.letter, numbers[i]])
 
+        return dots, dots_eat
